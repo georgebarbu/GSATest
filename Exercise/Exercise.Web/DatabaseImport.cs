da0fca60edd1f70f974fb67d9d97a4ae0697b967
George Barbu <georgebarbu@live.com>
2018-06-06 20:21:11 +0100

    more changes

---
+++ Exercise/Exercise.Web/DatabaseImport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Dapper;
+using CsvHelper;
+
+namespace Exercise.Web
+{
+    public class DatabaseImport : IDatabaseImport
+    {
+        public void InitializeDatabase()
+        {
+            // Create database, tables, and so on here
+        }
+
+        public void ImportStrategies()
+        {
+            
+        }
+
+        public void ImportCapital()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportPnL()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDatabaseImport
+    {
+        void InitializeDatabase();
+        void ImportStrategies();
+        void ImportCapital();
+        void ImportPnL();
+    }
+}
--- Exercise/Exercise.Web/Startup.cs
+++ Exercise/Exercise.Web/Startup.cs
@@ -16,7 +16,9 @@ namespace Exercise.Web
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+
+
+        }   
 
         public IConfiguration Configuration { get; }
 
